a090d025fc1a9a18e1a4142bb21f152095bb7204
Gašper Dobrovoljc <gasper.dobrovoljc1@gmail.com>
2024-08-14 23:08:11 +0200

    Filter draw methods by game id

--- src/routes/games/[gameId]/draw-methods/+page.server.ts
+++ src/routes/games/[gameId]/draw-methods/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { getDrawMethods } from '$lib/api';
 
-export const load: PageServerLoad = async () => {
-	const res = await getDrawMethods();
+export const load: PageServerLoad = async ({ params }) => {
+	const res = await getDrawMethods({ query: { gameId: params.gameId } });
 	return { drawMethods: res.data! };
 };
